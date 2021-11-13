b35cdf0e51f94198585c699b694e5c05d13c23ae
Derpinou <lightcompte@gmail.com>
2021-11-13 18:48:30 +0100

    :inbox_tray: Add ThreadManager

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
-export {TicketManager} from "./src/TicketManager";
+export {TicketManager} from "./src/Text/TicketManager";
+export {ThreadManager} from "./src/Thread/ThreadManager";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 export const version: string = require(`${__dirname}/../package.json`).version;
